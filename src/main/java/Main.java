f992120ccbb686fc645adfd027d41ca8223b3380
Milyukov Sergey <serrguk@gmail.om>
2023-05-19 00:13:58 +0700

    Read to Java object from JSON finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,5 +16,16 @@ public class Main {
         Person vasily = mapper.readValue(json, Person.class);
         System.out.println(vasily.getName());
         System.out.println(vasily.getJob().getSince());
+
+        vasily.setChildren(List.of("Ольга", "Олег"));
+
+        Car car = new Car();
+        car.setLicencePlate("O777OO124");
+        vasily.setCar(car);
+
+        String newJson = mapper.writeValueAsString(vasily);
+        try (FileWriter writer = new FileWriter("data/personMod.json")){
+            writer.write(newJson);
+        }
     }
 }

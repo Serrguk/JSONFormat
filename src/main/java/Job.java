9934e6197dede401007c8e62b6e1da3a28fc02f4
Sergey Milyukov <serrguk@gmail.com>
2023-05-19 10:08:19 +0700

    Method getSince in Job class modified for format date

--- src/main/java/Job.java
+++ src/main/java/Job.java
@@ -1,9 +1,16 @@
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class Job {
     private LocalDate since;
     private String city;
     private double salary;
+
+    public String getSince() {
+        return since.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
 }
+

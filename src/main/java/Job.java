a717a2ce1a95ff218465d457a5306ed5a530ca31
Sergey Milyukov <serrguk@gmail.com>
2023-05-19 10:08:19 +0700

    Method getSince in Job class modified for format date

--- src/main/java/Job.java
+++ src/main/java/Job.java
@@ -6,4 +6,13 @@ public class Job {
     private LocalDate since;
     private String city;
     private double salary;
+
+    public String getSince() {
+        String formatterMonth = String.valueOf(since.getMonth().getValue());
+        if (since.getMonth().getValue() < 10) {
+            formatterMonth = 0 + String.valueOf(since.getMonth().getValue());
+        }
+        return since.getDayOfMonth() + "-"+ formatterMonth + "-" + since.getYear();
+    }
 }
+
